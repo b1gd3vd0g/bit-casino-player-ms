da815cfa81b4691b3a573ac198e288db87ea856e
b1gd3vd0g <devin.peevy@outlook.com>
2025-07-22 21:34:32 -0700

    remove unused import.

--- src/models/user.rs
+++ src/models/user.rs
@@ -1,10 +1,10 @@
-use serde::{Deserialize, Serialize};
+use serde::Serialize;
 use uuid::Uuid;
 
 #[derive(sqlx::FromRow, Serialize)]
 pub struct User {
-  pub id: Uuid,
-  pub username: String,
-  pub email: String,
-  pub hashed_password: String
+    pub id: Uuid,
+    pub username: String,
+    pub email: String,
+    pub hashed_password: String,
 }

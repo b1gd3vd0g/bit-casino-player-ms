42b2d12dd0a5190fd5536d1b121e410db0e98df5
b1gd3vd0g <devin.peevy@outlook.com>
2025-07-23 16:06:22 -0700

    Extract register_user() function to its own module along with its structs.

---
+++ src/routes/registration.rs
@@ -0,0 +1,59 @@
+use axum::{extract::State, http::StatusCode, Json};
+use serde::{Deserialize, Serialize};
+use sqlx::PgPool;
+use uuid::Uuid;
+
+use crate::{hashing, models::user::User};
+
+#[derive(Deserialize)]
+pub struct RegistrationPayload {
+    username: String,
+    email: String,
+    password: String,
+}
+
+#[derive(Serialize)]
+pub struct RegistrationResponse {
+    id: Uuid,
+    username: String,
+    email: String,
+}
+
+/// Register a new user.
+pub async fn register_user(
+    State(pool): State<PgPool>,
+    Json(payload): Json<RegistrationPayload>,
+) -> Result<Json<RegistrationResponse>, (StatusCode, String)> {
+    let hashed = match hashing::hash_password(&payload.password) {
+        Ok(str) => str,
+        Err(_) => {
+            return Err((
+                StatusCode::INTERNAL_SERVER_ERROR,
+                String::from("Could not hash password."),
+            ))
+        }
+    };
+
+    let user = sqlx::query_as!(
+        User,
+        r#"
+        INSERT INTO gamblers (username, email, hashed_password)
+        VALUES ($1, $2, $3)
+        RETURNING id, username, email, hashed_password;
+        "#,
+        payload.username,
+        payload.email,
+        hashed
+    )
+    .fetch_one(&pool)
+    .await;
+
+    match user {
+        Ok(user) => Ok(Json(RegistrationResponse {
+            id: user.id,
+            username: user.username,
+            email: user.email,
+        })),
+        Err(e) => Err((StatusCode::INTERNAL_SERVER_ERROR, e.to_string())),
+    }
+}

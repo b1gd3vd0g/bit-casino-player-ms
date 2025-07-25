058675e5033a2d348bb490b2de266fcb9f8758b5
b1gd3vd0g <devin.peevy@outlook.com>
2025-07-24 18:08:18 -0700

    Export some structs and enums that are used across multiple handler functions.

---
+++ src/routes/responses.rs
@@ -0,0 +1,23 @@
+use serde::Serialize;
+
+/// This response can be returned from any handler when the request could not be completed, for
+/// any reason.
+#[derive(Serialize)]
+pub struct ErrorResponse {
+    /// A user friendly error message.
+    /// These may or may not be displayed frontend, and should not contain sensitive information.
+    pub message: String,
+}
+
+/// This enum should be returned by all handlers in this module.
+#[derive(Serialize)]
+#[serde(untagged)]
+pub enum ResponseBody<T: Serialize> {
+    Pass(T),
+    Fail(ErrorResponse),
+}
+
+/// This is returned from both the registration request and the sign in request.
+pub struct TokenResponse {
+    pub token: String,
+}

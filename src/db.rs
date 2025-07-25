c370e76625f586578a633c55ab0cc6051ee8b8e9
b1gd3vd0g <devin.peevy@outlook.com>
2025-07-25 12:08:23 -0700

    Restructure the database module to include models.

--- src/db.rs
+++ src/db.rs
@@ -1,3 +1,10 @@
+//! The db module contains a public `connect()` function at its root which generates a connection
+//! pool to our postgres database. It also contains the module `models` with all of our database
+//! models, as well as the module `queries` which contains functions to simplify making any
+//! necessary database queries.
+
+pub mod models;
+
 use sqlx::{postgres::PgPoolOptions, PgPool};
 use std::env;
 
---
+++ src/db/models.rs
@@ -0,0 +1,21 @@
+//! Contains all database models for the player microservice.
+//!
+//! For now, this only contains the `Player` model (found in the `players` table).
+//!
+//! Possible future enhancements include:
+//!
+//! - Adding a `LoginAttempt` model to track login attempts.
+
+use chrono::{DateTime, Utc};
+use sqlx::FromRow;
+use uuid::Uuid;
+
+/// The Player model represents a row from the `players` table in our database.
+#[derive(FromRow)]
+pub struct Player {
+    pub id: Uuid,
+    pub username: String,
+    pub email: String,
+    pub hashed_password: String,
+    pub created_at: DateTime<Utc>,
+}

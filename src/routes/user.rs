11acac189fcdc81d444b0f2365675df07339b853
b1gd3vd0g <devin.peevy@outlook.com>
2025-07-25 10:47:49 -0700

    Change paths to match the documentation. Added the deletion handler to the base path.

--- src/routes/user.rs
+++ src/routes/user.rs
@@ -1,11 +1,14 @@
 use axum::{routing::post, Router};
 use sqlx::PgPool;
 
-use crate::routes::{login, registration};
+use crate::routes::{deletion, login, registration};
 
 /// The router handling all user paths.
 pub fn user_routes() -> Router<PgPool> {
     Router::new()
-        .route("/register", post(registration::register_user))
+        .route(
+            "/",
+            post(registration::register_user).delete(deletion::delete_player),
+        )
         .route("/authn", post(login::validate_login))
 }

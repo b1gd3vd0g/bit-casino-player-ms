1f5d7fc16d582f19b82f97e28f5905051213854e
b1gd3vd0g <devin.peevy@outlook.com>
2025-07-25 19:24:02 -0700

    Use the address 0.0.0.0 instead of 127.0.0.1 - this ensures that the application can be reached from outside the docker container.

--- src/main.rs
+++ src/main.rs
@@ -16,7 +16,7 @@ async fn main() {
     let db_pool = db::connect().await;
     let app = router().with_state(db_pool);
 
-    let address = SocketAddr::from(([127, 0, 0, 1], 3000));
+    let address = SocketAddr::from(([0, 0, 0, 0], 3000));
     let listener = tokio::net::TcpListener::bind(address).await.unwrap();
     println!("Listening on {}", address.to_string());
     axum::serve(listener, app).await.unwrap();

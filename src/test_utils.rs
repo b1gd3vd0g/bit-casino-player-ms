7161ea9585ecdf199fbd5f89c9209583d01229c9
b1gd3vd0g <devin.peevy@outlook.com>
2025-07-29 09:21:06 -0700

    get rid of compiler warning by allowing dead code (it isnt dead, its just used for tests!)

--- src/test_utils.rs
+++ src/test_utils.rs
@@ -2,9 +2,11 @@ use std::sync::Once;
 
 use dotenv::dotenv;
 
-static INIT: Once = Once::new();
+#[allow(dead_code)]
+static INIT: Once = Once::new(); // Not dead
 
 /// Initializes the environment for testing.
+#[allow(dead_code)] // This is not dead code, just used in tests.
 pub fn test_setup() {
     INIT.call_once(|| {
         dotenv().ok();

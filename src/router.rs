8a7f7abd67192401b07669e4a13039169d2beca8
b1gd3vd0g <devin.peevy@outlook.com>
2025-07-29 09:19:41 -0700

    add the documentation route.

--- src/router.rs
+++ src/router.rs
@@ -8,13 +8,16 @@ use crate::handlers::{
     authentication::{login::handle_login, token::handle_fetch_player_by_token},
     creation::handle_player_creation,
     deletion::handle_player_deletion,
+    documentation::handle_serve_documentation,
 };
 
 pub fn router() -> Router<PgPool> {
     Router::new()
         .route(
             "/",
-            post(handle_player_creation).delete(handle_player_deletion),
+            post(handle_player_creation)
+                .delete(handle_player_deletion)
+                .get(handle_serve_documentation),
         )
         .route(
             "/authn",
